6f725bb2adccf89fea0d8f39820c99321f54dc0b
vinhnlk <vinhnlk@ghn.vn>
2019-12-18 21:38:52 +0700

    fix

--- route/route.go
+++ route/route.go
@@ -7,9 +7,10 @@ import (
 
 // CrawlRoute ...
 func CrawlRoute(e *echo.Echo) {
+	// Api để crawl và migrate data
 	// e.GET("/", handler.CrawlData)
 	// e.GET("/get-all", handler.MoveDataBase1)
-	e.GET("/migrate-database-2", handler.MigrateToDataBase2)
+	// e.GET("/migrate-database-2", handler.MigrateToDataBase2)
 	e.GET("/get/all", handler.GetAll)
 	e.GET("/get-by-name", handler.GetByName)
 	e.GET("/get-by-phone", handler.GetByPhone)
