b8cbd0e6ec19195050bdd01859544e928b620960
vinhnlk <vinhnlk@ghn.vn>
2019-12-18 22:02:57 +0700

    add get all database1

--- route/route.go
+++ route/route.go
@@ -11,8 +11,8 @@ func CrawlRoute(e *echo.Echo) {
 	// e.GET("/", handler.CrawlData)
 	// e.GET("/get-all", handler.MoveDataBase1)
 	// e.GET("/migrate-database-2", handler.MigrateToDataBase2)
-	e.GET("/get/all", handler.GetAllData1)
-	e.GET("/get/all", handler.GetAll)
+	e.GET("/get/all/database-1", handler.GetAllData1)
+	e.GET("/get/all/database-2", handler.GetAll)
 	e.GET("/get-by-name", handler.GetByName)
 	e.GET("/get-by-phone", handler.GetByPhone)
 
